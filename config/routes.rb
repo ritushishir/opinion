bb87c6b299aeb931dd3dcbbfea009e38357f1e44
a5his <commit@a5his.com>
2013-04-21 09:53:50 +0545

    changed root url to opinion list

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Opinion::Application.routes.draw do
   resources :criticisms, :controller =>'opinion', :path =>'opinions'
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'opinion#index'
+   root :to => 'opinion#list'
 
   # See how all your routes lay out with "rake routes"
 
